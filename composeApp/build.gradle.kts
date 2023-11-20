e35015a5326564ad591335c36205c3d4afc3214f
ISNing <1208733578@qq.com>
2023-11-20 22:59:25 +0800

    Migrate to Material 3

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -83,7 +83,6 @@ kotlin {
             implementation(projects.shared)
             implementation(compose.runtime)
             implementation(compose.foundation)
-            implementation(compose.material)
             implementation(compose.material3)
             @OptIn(ExperimentalComposeLibrary::class)
             implementation(compose.components.resources)
