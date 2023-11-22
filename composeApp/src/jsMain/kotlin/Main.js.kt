a49b33f204a89e3cb525add33489e6dffa9e95da
ISNing <1208733578@qq.com>
2023-11-22 19:59:47 +0800

    Add temporarily workaround for browser touch scrolling

--- composeApp/src/jsMain/kotlin/Main.js.kt
+++ composeApp/src/jsMain/kotlin/Main.js.kt
@@ -1,12 +1,27 @@
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.CanvasBasedWindow
+import cn.edu.buct.snc.xware.manage.App
+import kotlinx.browser.document
+import kotlinx.browser.window
 import org.jetbrains.skiko.wasm.onWasmReady
+import org.w3c.dom.HTMLCanvasElement
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
+    window.onload = {
+        // Initialize touch handler for the canvas element
+        initTouchHandler(document.getElementById("ComposeTarget") as HTMLCanvasElement? ?: error("No canvas element found!"))
+    }
     onWasmReady {
-        CanvasBasedWindow {
+        CanvasBasedWindow("Compose Wow", canvasElementId = "ComposeTarget") {
             App()
         }
     }
+}
+
+@OptIn(ExperimentalJsExport::class)
+@JsExport
+@JsName("initTouchHandler")
+fun initTouchHandler(element: HTMLCanvasElement) {
+    js("initTouchHandler(element)")
 }
