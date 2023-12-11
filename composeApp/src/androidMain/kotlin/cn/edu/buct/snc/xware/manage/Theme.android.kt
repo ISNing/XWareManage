7c3d63ce93efa6bc169f2c33c5c8be3511f59bb4
ISNing <1208733578@qq.com>
2023-12-11 12:15:22 +0800

    Add workaround for async loading sc fonts

---
+++ composeApp/src/androidMain/kotlin/cn/edu/buct/snc/xware/manage/Theme.android.kt
@@ -0,0 +1,26 @@
+package cn.edu.buct.snc.xware.manage
+
+import android.annotation.SuppressLint
+import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
+import androidx.compose.material3.Typography
+import androidx.compose.runtime.Composable
+
+@Composable
+actual fun PlatformSpecifiedAppTheme(
+    colorScheme: ColorScheme,
+    shapes: Shapes,
+    typography: Typography,
+    content: @Composable () -> Unit
+) = MaterialTheme(
+    colorScheme = colorScheme,
+    shapes = shapes,
+    typography = typography,
+    content = content
+)
+
+@SuppressLint("ComposableNaming")
+@Composable
+actual fun platformComposableInit() {
+}
---
+++ composeApp/src/commonMain/kotlin/cn/edu/buct/snc/xware/manage/App.kt
@@ -0,0 +1,9 @@
+package cn.edu.buct.snc.xware.manage
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun App() {
+    AppTheme {
+    }
+}
